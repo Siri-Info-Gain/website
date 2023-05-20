533bcd12108b55dd37afb193e04245a80e437037
Siri-Info-Gain <128180329+Siri-Info-Gain@users.noreply.github.com>
2023-05-20 15:46:17 -0400

    Add files via upload

---
+++ src/components/Construction.jsx
@@ -0,0 +1,8 @@
+import { constructionImg } from "../assets";
+//import styles from "../style";
+
+const Construction = () => (
+    <img src={constructionImg} alt="client_logo" className="w-full object-contain" />
+);
+
+export default Construction;
