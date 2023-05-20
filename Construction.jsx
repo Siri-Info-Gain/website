1dfee71f9fada2d9a79cbbbfc7bce95ef2e67161
Siri-Info-Gain <128180329+Siri-Info-Gain@users.noreply.github.com>
2023-05-20 15:45:28 -0400

    Delete Construction.jsx

--- Construction.jsx
+++
@@ -1,8 +0,0 @@
-import { constructionImg } from "../assets";
-//import styles from "../style";
-
-const Construction = () => (
-    <img src={constructionImg} alt="client_logo" className="w-full object-contain" />
-);
-
-export default Construction;
